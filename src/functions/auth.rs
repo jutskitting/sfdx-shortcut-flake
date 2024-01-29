f25bcf7d8bb06033f724a920d20528ec2fafaff7
kit <kitherman@gmail.com>
2024-01-29 08:52:58 -0500

    bugfix

--- src/functions/auth.rs
+++ src/functions/auth.rs
@@ -6,9 +6,7 @@ pub fn auth(dir:&str) -> Result<ExitStatus, io::Error>{
     println!("working dir : {}",dir);
 
     let mut child = Command::new("npx")
-        .current_dir(dir)
         .args(["sf","org","login","web","-r https://test.salesforce.com","-s"])
-        .env("PATH","/home/kit/Documents/SalesForce/Work")
         .spawn()?;
 
     child.wait()
